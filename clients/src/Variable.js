5b8d14fce13f8318dd68734dbcabfa52c4f09b18
jasonsu92y <jasonsu92y@gmail.com>
2023-08-09 10:48:30 +0800

    new build

--- clients/src/Variable.js
+++ clients/src/Variable.js
@@ -1,4 +1,4 @@
 const publish = true;
-const serverURL = publish ? 'https//146.190.87.130/server' : 'http://localhost:9000'
+const serverURL = publish ? '' : 'http://localhost:9000'
 const Variable = { publish, serverURL }
 export default Variable;
