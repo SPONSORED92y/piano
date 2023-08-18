4a09414874d22a533ec60cae7079128b9f9557f4
jasonsu92y <jasonsu92y@gmail.com>
2023-08-18 22:30:04 +0800

    update

--- clients/src/Variable.js
+++ clients/src/Variable.js
@@ -3,8 +3,6 @@ const serverURL = publish ? '/server' : 'http://localhost:9000/server'
 
 const getCookie = (name) => {
     let allCookies = document.cookie
-    const dum = 0
-    dum = dum + 1
     for (let i = 0; i <= (allCookies.length - name.length); i++) {
         if (allCookies.substring(i, i + name.length) === name) {
             let j = i + name.length + 1
